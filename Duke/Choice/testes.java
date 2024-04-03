93df78816dd58f15638ae8a7be8ed6a0402bcae8
NLdouglas <douglasleodoro.qi@gmail.com>
2024-04-03 18:50:21 -0300

    loops

--- Duke/Choice/testes.java
+++ Duke/Choice/testes.java
@@ -1,32 +1,20 @@
 package Duke.Choice;
 
 public class testes {
-    public static void main (String [] args ){
+    public static void main (String [] args ) {
 
-<<<<<<< HEAD
-        int [] ages1 = new int [3];
-        ages1[0] = 19;
-        ages1[1] = 15;
-        ages1[2] = 14;
-<<<<<<< HEAD
-        ages1[3] = 54; // out of bounds
-=======
-
->>>>>>> 31015f3e9116c83b08de78de2bf3d2b4f1068383
-
-
-        System.out.println(ages1);
-=======
-        System.out.println("Test" + args[0]);
->>>>>>> 6d7e11151a068c8b68572c6883b0e740d64a4699
-
-
-
-
+        int passmark = 12;
+        boolean passed = false;
+        int [] scores = { 4, 6, 2, 8, 12, 35};
+        for( int i : scores) {
+            if (i >= 12) {
+                passed = true;
+                break;
+            }
         }
+        System.out.println("At least one passed? " +passed);
 
-
-
-        // arrays
     }
+
+}
 
