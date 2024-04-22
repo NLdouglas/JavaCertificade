de3b397eff752cc5291e83a26463df9818e6fd22
NLdouglas <douglasleodoro.qi@gmail.com>
2024-04-22 00:45:47 -0300

    end of Loop and conditions

--- Duke/Choice/ShopApp.java
+++ Duke/Choice/ShopApp.java
@@ -5,7 +5,7 @@ public class ShopApp {
     public static void main(String [] args) {
 
         double tax = 0.2;
-        double total;
+        double total = 0.0;
         int measurement = 3;
 
         System.out.println("Welcome to duke Choice Shop");
@@ -14,23 +14,45 @@ public class ShopApp {
         c1.name = "Pink";
         c1.size = "S";
 
-
-        
         Clothing item1 = new Clothing();
         Clothing item2 = new Clothing();
-        item1.description = "Blue Jacket";
-        Clothing [] items = {item1, item2} ;
+        Clothing item3 = new Clothing();
 
+        Clothing [] items = {item1, item2, item3, new Clothing()} ;
+        item1.description = "Blue Jacket";
         item1.price = 20.9;
         item1.size = "M";
 
-
-
-        //other way do call size
-        //item1.size = "M"
         item2.description = "Orange T-Shirt";
         item2.price = 10.5;
         item2.size = "S";
+
+        item3.description = "Green scarf";
+        item3.price = 5;
+        item3.size = "S";
+
+        // Adicionando valor ao index da lista de objetos Clothing
+        items[3].description = "Green Scarf";
+        items[3].price = 10.5;
+        items[3].size = "S";
+
+        System.out.println("Customer is " + c1.name);
+//        for (Clothing itens: items){
+//            System.out.println("Clothing " + itens.description + ", price is " + itens.price + ", and the size is " + itens.size);
+//
+//        }
+
+        for (Clothing itens: items ){
+            if (c1.size.equals(itens.size)) {
+                total += itens.price;
+                System.out.println("Clothing " + itens.description + ", price is " + itens.price + ", and the size is " + itens.size);
+                if (total > 15){ break;}
+
+            }
+
+        }
+
+        System.out.println(total);
 
         switch (measurement) {
             case 1:
@@ -54,11 +76,11 @@ public class ShopApp {
         }
 
 
-        System.out.println("Customer is " + c1.name);
-        System.out.println("Clothing " + item1.description + ", price is " + item1.price + "and size is " + item1.size);
-        System.out.println("Clothing " + item2.description + ", price is " + item2.price + "and size is " + item2.size);
-        total = (item1.price + item2.price * 2) + (1 + tax);
-        System.out.println(total);
+
+
+
+//        total = (item1.price + item2.price * 2) + (1 + tax);
+
     }
 
 }
--- Duke/Choice/testes.java
+++ Duke/Choice/testes.java
@@ -3,7 +3,7 @@ package Duke.Choice;
 public class testes {
     public static void main (String [] args ){
 
-<<<<<<< HEAD
+
         int [] ages1 = new int [3];
         ages1[0] = 19;
         ages1[1] = 15;
@@ -12,9 +12,9 @@ public class testes {
 
 
         System.out.println(ages1);
-=======
+
         System.out.println("Test" + args[0]);
->>>>>>> 6d7e11151a068c8b68572c6883b0e740d64a4699
+
 
 
 
