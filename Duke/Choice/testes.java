0f695b03a02cf5ecf147642fe8e6cf07c4558680
NLdouglas <douglasleodoro.qi@gmail.com>
2024-04-23 00:42:34 -0300

    Defining Classes and Creating Objects

--- Duke/Choice/testes.java
+++ Duke/Choice/testes.java
@@ -1,40 +1,21 @@
 package Duke.Choice;
 
-public class testes {
-<<<<<<< HEAD
-    public static void main (String [] args ){
 
 
-        int [] ages1 = new int [3];
-        ages1[0] = 19;
-        ages1[1] = 15;
-        ages1[2] = 14;
+    class Customers {
 
+        String name;               // Field Definition
+        String size = "S";
 
+        void setSize(String newSize) {
+            size = newSize;
+        }                              //Method definition
 
-        System.out.println(ages1);
-
-        System.out.println("Test" + args[0]);
-
-
-
-
-=======
-    public static void main (String [] args ) {
->>>>>>> 93df78816dd58f15638ae8a7be8ed6a0402bcae8
-
-        int passmark = 12;
-        boolean passed = false;
-        int [] scores = { 4, 6, 2, 8, 12, 35};
-        for( int i : scores) {
-            if (i >= 12) {
-                passed = true;
-                break;
-            }
+        String getSize() {
+            return size;
         }
-        System.out.println("At least one passed? " +passed);
-
     }
 
-}
+
+
 
