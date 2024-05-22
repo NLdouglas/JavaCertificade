f3ed4767e0113ed00c87b50472ac4418baaed5e4
NLdouglas <douglasleodoro.qi@gmail.com>
2024-05-21 22:04:41 -0300

    att

--- Duke/Choice/Customer.java
+++ Duke/Choice/Customer.java
@@ -4,5 +4,19 @@ public class Customer {
     String name;
     String size = "S";
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
--- Duke/Choice/testes.java
+++ Duke/Choice/testes.java
@@ -2,17 +2,10 @@ package Duke.Choice;
 
 
 public class testes {
-    public void printWishList () {
-        String[] list = {"lalal", "lala2"};
-        
-        for (String idea: list) {
-            System.out.println(idea);
-        }
-    }
 
-    public double getTotal (double price, int quantity) {
-        double total = price*quantity;//tax;
-        return total;
+    double price;
+    public void setPrice ( String price) {
+        this.price = price();
     }
 
 }
